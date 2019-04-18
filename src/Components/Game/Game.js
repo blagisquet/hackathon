906e1215331802cc7f1222154cd60b58b65e7a5c
Aurelia Roumesy <aurelia.roumesy@gmail.com>
2019-04-18 20:24:22 +0200

    ternaire homepage ok

---
+++ src/Components/Game/Game.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Game.css';
+import Enemy from './Enemy/Enemy';
+import Player from './Player/Player';
+
+
+const Game = () => {
+   return (
+     <div>
+       <p>kzergvblhiegvb</p>
+       {/* <Enemy />
+       <Player /> */}
+     </div>
+   );
+ }
+
+
+export default Game;
